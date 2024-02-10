a1a1534917821929c72f8241666131df0ab3ba3d
maritocuate <mario300@gmail.com>
2024-02-10 12:56:46 -0300

    chore: root page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@ import Foundraising from './components/Foundraising'
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex flex-col justify-center items-center">
+    <main className="mx-auto max-w-7xl md:p-3">
+      <div className="mx-auto w-full max-w-screen-xl px-2.5 my-5 md:mb-10 md:mt-0 flex flex-col items-center justify-center text-center">
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Ayudanos a salvar <span className="text-primary">patitas</span>.
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+          Juntamos fondos para las causas mas votadas. Una manera de
+          organizarnos con los que mas lo necesitan.
+        </p>
+      </div>
       <Foundraising />
-      <h1>Salvemos</h1>
     </main>
   )
 }
