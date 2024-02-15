248bdc10c7b8696e089811c1649ba6ed9b94cedc
maritocuate <mario300@gmail.com>
2024-02-15 19:56:02 -0300

    chore: you page

---
+++ src/app/you/page.tsx
@@ -0,0 +1,31 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+export default function Page() {
+  return (
+    <div className="mx-auto w-full px-2.5 md:px-20 md:mt-3 mb-8 mt-14 text-center max-w-5xl">
+      <div className="mx-auto mb-6 sm:max-w-xl">
+        <h1 className="text-6xl font-bold sm:text-7xl">Ustedes.</h1>
+        <ScrollArea className="h-100 mt-9 rounded-md border">
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+          <div className="h-30">asd</div>
+        </ScrollArea>
+      </div>
+    </div>
+  )
+}
